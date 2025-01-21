c8b91f60ddd0fcceded21e5237b658e4973de944
katr1na <katrinango3388@gmail.com>
2025-01-20 22:18:01 -0700

    added gallery file

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,15 @@
+import "../styles.css";
+
+const Gallery = () => {
+  return (
+    <div>
+      <iframe
+        src="https://widget.tagembed.com/2148638"
+        style={{ border: "none", width: "100%", height: "600px" }}
+      ></iframe>
+      
+    </div>
+  );
+};
+
+export default Gallery;
